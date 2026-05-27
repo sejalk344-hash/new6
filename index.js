ceb23b0db4daa7700fcb53776793ebe8961d44b7
sejalk344-hash <sejalk344@gmail.com>
2026-05-27 22:19:46 +0530

    Add files via upload
    tru ttfuf

---
+++ index.js
@@ -0,0 +1,10 @@
+
+function whatsapp(){
+    mainpage.style.display="none"
+    ndpage1.style.display="block"
+
+}
+
+function page2nd() {
+    mainpage.style.display = "none";
+    ndpage.style.display = "block"}
